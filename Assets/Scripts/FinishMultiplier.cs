673cacbc4be2d8156b5c3fb91236606dd905cd12
melihsapmazz <melih221@gmail.com>
2023-03-23 21:20:34 +0300

    Finish multiplier and camera

---
+++ Assets/Scripts/FinishMultiplier.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FinishMultiplier : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private int multiplier = 1;
+    private void OnTriggerEnter(Collider other) {
+        //Bıçağı saplama eklenecek.
+        if(this.gameObject.tag != "Empty"){
+            text = this.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+            string str = text.ToString();
+            str.Remove(str.Length-1);
+            multiplier = int.Parse(str);
+        }
+            GameManager.Instance.UpdateMoney(GameManager.Instance.Money*multiplier);
+            this.transform.parent.GetChild(0).GetComponent<ParticleSystem>().Play();
+            this.transform.parent.GetChild(1).gameObject.SetActive(true);
+            StartCoroutine(FinishUI());
+    }
+    IEnumerator FinishUI(){
+        yield return new WaitForSeconds(2);
+        GameManager.Instance.FinishLevel();
+    }
+}
